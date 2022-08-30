ec4500fa252fc1d09ce37b41b7a149aad8b2a6da
Andrewcodes12 <andrewfava@yahoo.com>
2022-08-30 13:15:30 -0400

    add event/get event by id routes done

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -1,12 +1,8 @@
-import datetime
-import os
-from sqlite3 import Date
-
 from flask import Blueprint, jsonify, request, Flask
 from flask_login import current_user, login_required
 from app.models import db, User, Event, Review
 from flask_sqlalchemy import SQLAlchemy
-
+from app.forms import new_event_form
 
 event_routes = Blueprint('events', __name__)
 
@@ -14,5 +10,50 @@ event_routes = Blueprint('events', __name__)
 # Get all events
 @event_routes.route('/', methods=['GET'])
 def get_events():
+    """
+    Get all events
+    """
     events = Event.query.all()
     return jsonify([event.to_dict() for event in events])
+
+
+
+# Get event by id
+@event_routes.route('/<int:id>', methods=['GET'])
+def get_event(id):
+    """
+    Get event by id
+    """
+    event = Event.query.get(id)
+    return jsonify(event.to_dict())
+
+
+# Create event
+@event_routes.route('/', methods=['POST'])
+def create_event():
+    """
+    Create event
+    """
+    form = new_event_form.EventForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        event = Event(
+            title=form.data['title'],
+            event_description=form.data['event_description'],
+            category=form.data['category'],
+            event_city=form.data['event_city'],
+            event_state=form.data['event_state'],
+            event_zipcode=form.data['event_zipcode'],
+            event_date=form.data['event_date'],
+            event_imgs=form.data['event_imgs'],
+            user_id=form.data['user_id']
+        )
+        db.session.add(event)
+        db.session.commit()
+        return event.to_dict()
+
+    return jsonify(form.errors)
+
+
+
---
+++ app/forms/new_event_form.py
@@ -0,0 +1,24 @@
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, IntegerField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+class EventForm(FlaskForm):
+    title = StringField('Event Title', validators=[DataRequired()])
+    event_description = TextAreaField('Description', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('', 'Select Category'), ('Concert', 'Concert'), ('Sports', 'Sports'), ('Party', 'Party'),('Video-Games', 'Video-Games'), ('Other', 'Other')], validators=[DataRequired()])
+    event_city = StringField('City', validators=[DataRequired()])
+    event_state = StringField('State', validators=[DataRequired()])
+    event_zipcode = IntegerField('Zipcode', validators=[DataRequired()])
+    event_date = DateField('Date', validators=[DataRequired()])
+    event_imgs = StringField('Event Images', validators=[DataRequired()])
+    user_id = IntegerField('User ID', validators=[DataRequired()])
+
+    def validate_start_time(self, start_time):
+        if start_time.data < datetime.datetime.now():
+            raise ValidationError('Start time must be in the future.')
+
+    def validate_end_time(self, end_time):
+        if end_time.data < datetime.datetime.now():
+            raise ValidationError('End time must be in the future.')
