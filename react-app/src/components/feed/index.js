c199673fd53d59a9e8271df1914b1149c8971aa4
Andrewcodes12 <andrewfava@yahoo.com>
2022-08-31 13:39:51 -0400

    all stores are fully opertaional, time to work on the front end

--- react-app/src/components/feed/index.js
+++ react-app/src/components/feed/index.js
@@ -3,12 +3,13 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import {loadEvents} from '../../store/event';
 import {loadAllReviews} from '../../store/review';
+import {setUser} from '../../store/session';
 
 function Feed() {
 const dispatch = useDispatch();
 const events = useSelector(state => state.events.events);
 const reviews = useSelector(state => state.reviews.reviews);
-
+const user = useSelector(state => state.session.user);
 
 useEffect(() => {
     dispatch(loadEvents());
