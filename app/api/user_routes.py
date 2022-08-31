6b7bb050387f305559ad69d4ee1e5cff49d72bbd
Andrewcodes12 <andrewfava@yahoo.com>
2022-08-31 12:41:37 -0400

    edit user route done

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import db, User
+from app.forms import signup_form
 
 user_routes = Blueprint('users', __name__)
 
@@ -26,3 +27,28 @@ def user_events(id):
     user = User.query.get(id)
     return {'events': [event.to_dict() for event in user.events]}
 
+# edit user profile
+@user_routes.route('/<int:id>/edit', methods=['PUT'])
+# @login_required
+def edit_user(id):
+    """
+    Edit user profile
+    """
+    form = signup_form.SignUpForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        user = User.query.get(id)
+        user.username = form.data['username']
+        user.email = form.data['email']
+        user.hashed_password = form.data['password']
+        user.first_name = form.data['first_name']
+        user.last_name = form.data['last_name']
+        user.city = form.data['city']
+        user.state = form.data['state']
+        user.zipcode = form.data['zipcode']
+        user.user_image = form.data['user_image']
+        user.user_bio = form.data['user_bio']
+
+        db.session.commit()
+        return user.to_dict()
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, DateField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -21,7 +21,13 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
+    username = StringField('username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state = StringField('state', validators=[DataRequired()])
+    zipcode = IntegerField('zipcode', validators=[DataRequired()])
+    user_image = StringField('user_image', validators=[DataRequired()])
+    user_bio = TextAreaField('user_bio', validators=[DataRequired()])
