5e29515b734d0d60fe2070016e92b03a62b95ef6
Andrewcodes12 <andrewfava@yahoo.com>
2022-09-22 11:44:57 -0400

    event by category is styled

--- react-app/src/components/EventByCategory/EventByCategory.js
+++ react-app/src/components/EventByCategory/EventByCategory.js
@@ -20,25 +20,30 @@ function EventByCategory() {
   return (
     <>
     <Categories />
-    {events && events.map(event => (
-        event.category === category ? (
+    <div className="feed">
+      <div className="feed-events-container">
+        {events && events.map(event => (
+            event.category === category ? (
+                <div className="feed-event" key={event.id}>
+                <NavLink to={`/events/${event.id}`} className="event-link">
+                  <div className="feed-event-img">
+                    <img src={event.event_imgs} alt="event-img" />
+                  </div>
+                  <div className="feed-event-info">
+                    <h3 className="event-title">{event.title}</h3>
+                    <p className="event-description">{event.event_description}</p>
+                    <p className="event-date">{event.event_date}</p>
+                    <p className="event-location">{event.event_city}, {event.event_state} {event.event_zipcode}</p>
+                    <p className="event-category">{event.category}</p>
+                  </div>
+                </NavLink>
+                </div>
+            ) :
             <>
-            <NavLink to={`/events/${event.id}`} className="event-link">
-              <h1>{event.title}</h1>
-              <img src={event.event_imgs} alt="event-img" />
-              <p className="category-event-description">{event.event_description}</p>
-              <p>{event.event_date}</p>
-              <p>{event.event_city}, {event.event_state} {event.event_zipcode}</p>
-              <p>{event.category}</p>
-            </NavLink>
             </>
-        ) :
-        <>
-        </>
-    ))}
-
-
-
+        ))}
+      </div>
+  </div>
     </>
   )
 }
