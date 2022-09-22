bf2b1b859a62a6ea8b5ebe5fa3d520f5d3381cfb
Andrewcodes12 <andrewfava@yahoo.com>
2022-09-22 13:52:51 -0400

    user public profile almost done

--- react-app/src/components/UserProfile/UserProfile.js
+++ react-app/src/components/UserProfile/UserProfile.js
@@ -41,31 +41,41 @@ function goToEvent(eventId) {
     <>
     <NavBar />
     <div className='user-profile-container'>
+
       {users && users.map(user => (
         user.id === parseInt(userId) ? (
           <>
-          <h1 className="user-profile-name">{user.username}</h1>
-          <img src={user.user_image} alt="user-img" className="user-profile-img"/>
-          <p className="user-profile-bio">{user.user_bio}</p>
-          <p className="user-profile-location">{user.city}, {user.state} {user.zipcode}</p>
+          <div className='user-profile-info'>
+          <h1 className="user-profile-name">Hi, I'm {user.username}</h1>
+            <EdituserProfile user={user} className="user-profile-edit"/>
+            <img src={user.user_image} alt="user-img" className="user-profile-img"/>
+            <p className="user-profile-bio">{user.user_bio}</p>
+            <p className="user-profile-location">Lives in {user.city}, {user.state}</p>
+            <div className="a">
+            </div>
+          </div>
 
           <div className="user-profile-btns">
-            <EdituserProfile user={user}/>
+            <h3>Events I'm Hosting</h3>
             <button onClick={handleFilterByDate}>Filter by Date</button>
           </div>
 
-          <div className="user-profile-events">
+          <div className="feed-events-container">
             {events && events.map(event => (
               user.id === event.user_id ? (
-                <div className="user-profile-event" key={user.id}>
-
-                    <h4 className="user-event-title">{event.title}</h4>
-                    <img src={event.event_imgs} alt="profile-img" className="user-event-img" onClick={() => goToEvent(event.id)}/>
-                    <p className="user-event-description">{event.event_description}</p>
-                    <p className="user-event-date">{event.event_date}</p>
-                    <p className="user-event-location">{event.event_city}, {event.event_state} {event.event_zipcode}</p>
-                    <p className="user-event-category">{event.category}</p>
-
+                <div className="feed-event" key={user.id}>
+                  <NavLink to={`/events/${event.id}`} className="event-link" key={event.id} onClick={() => goToEvent(event.id)}>
+                    <div className="feed-event-img">
+                      <img src={event.event_imgs} alt="profile-img" className="feed-event-img" />
+                    </div>
+                  <div className="feed-event-info">
+                      <h4 className="event-title">{event.title}</h4>
+                      <p className="event-description">{event.event_description}</p>
+                      <p className="event-date">{event.event_date}</p>
+                      <p className="event-location">{event.event_city}, {event.event_state} {event.event_zipcode}</p>
+                      <p className="event-category">{event.category}</p>
+                  </div>
+                  </NavLink>
                 </div>
               ) : null
             ))}
